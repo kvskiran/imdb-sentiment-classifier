e7d0e8ed92b96ecf1c19f5e59144b3f19bcd4fb7
Matt Usifer <mattusifer@gmail.com>
2016-04-02 13:37:31 -0400

    Adding processed files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
@@ -32,39 +33,63 @@ public class Main {
         }
     }
 
-    public static void main(String args [ ]) {
-        // test files
-        String baseDir = "resources/aclImdb/";
-        File[] testPos = new File(baseDir + "test/pos").listFiles();
-        String testStr = "";
+    public static void process(File file, Lemmatizer lem, Stemmer stem)  
+    {
+        String fileStr = "";
+        File processedFile = new File(file.getAbsolutePath().replace("resources","resources/processed"));
 
         try {
-            testStr = Util.readFile(testPos[6].getAbsolutePath(), Charset.forName("UTF-8")).toLowerCase();
-        } catch (IOException e) {}
-
-        // init
-        Lemmatizer lem = new Lemmatizer();
-        Stemmer stem = new Stemmer();
-
-        // print raw string
-        System.out.println("\n###### RAW STR ######");
-        System.out.println(testStr);
-
+            fileStr = Util.readFile(file.getAbsolutePath(), Charset.forName("UTF-8")).toLowerCase();
+            processedFile.getParentFile().mkdirs();
+            processedFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+                
         // lemmatize
-        List<String> lemmas = lem.lemmatize(testStr);
-
-        // print lemmatized string
-        System.out.println("\n###### LEMMATIZED ######");
-        System.out.println(Util.concatList(lemmas));
+        List<String> lemmas = lem.lemmatize(fileStr);
 
         // remove stopwords and stem
         List<String> lemmasWithoutStopwords = StopwordRemover.removeStopwords(lemmas);
         List<String> stems = stem.stemStr(Util.removeSpecialCharacters(Util.concatList(lemmasWithoutStopwords)));
 
-        // print stemmed string
-        System.out.println("\n###### STEMMED ######");
-        System.out.println(Util.concatList(stems));
+        // write processed str to new file
+        try {
+            PrintStream ps = new PrintStream(processedFile.getAbsolutePath());
+            ps.println(Util.concatList(stems));
+        }
+        catch (Exception e) { }
+    }
+        
+    public static void main(String args [ ]) {
+        // test files
+        String baseDir = "resources/aclImdb/";
+        File[] trainPos = new File(baseDir + "train/pos").listFiles();
+        File[] trainNeg = new File(baseDir + "train/neg").listFiles();
+        File[] testPos = new File(baseDir + "train/pos").listFiles();
+        File[] testNeg = new File(baseDir + "train/neg").listFiles();        
 
-        // demoVocabBuild(lem, Arrays.asList(testPos));
+        Lemmatizer lem = new Lemmatizer();
+        Stemmer stem = new Stemmer();
+
+        for (File f : trainPos) {
+            process(f, lem, stem);
+            System.out.println(f.getAbsolutePath() + " processed.");
+        }
+
+        for (File f : trainNeg) {
+            process(f, lem, stem);
+            System.out.println(f.getAbsolutePath() + " processed.");
+        }
+
+        for (File f : testPos) {
+            process(f, lem, stem);
+            System.out.println(f.getAbsolutePath() + " processed.");
+        }
+
+        for (File f : testNeg) {
+            process(f, lem, stem);
+            System.out.println(f.getAbsolutePath() + " processed.");
+        }
     }
 }
