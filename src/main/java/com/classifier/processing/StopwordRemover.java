cc096361642b840d8733652540fc334b2ecfefd4
Matt Usifer <mattusifer@gmail.com>
2016-04-04 14:16:28 -0400

    Update StopwordRemover.java

--- src/main/java/com/classifier/processing/StopwordRemover.java
+++ src/main/java/com/classifier/processing/StopwordRemover.java
@@ -9,7 +9,8 @@ import com.classifier.utilities.Util;
 public class StopwordRemover {    
     public static List<String> removeStopwords(List<String> input) {
         try {
-            List<String> stopwords = Util.readFileByLine("src/main/resources/aclImdb/stopwords.txt");
+            List<String> stopwords = Util.readFileByLine("src" + File.separator + "main" + File.separator 
+                + "resources" + File.separator + "aclImdb" + File.separator + "stopwords.txt");
             List<String> output = new ArrayList<String>();
             for (String word : input) {
                 if (!stopwords.contains(word)) {
